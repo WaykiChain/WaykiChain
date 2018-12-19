10cb0ed3d63911014bd39f4ea34c5c27bf8b09c0
Dragon Master <tigerich@users.noreply.github.com>
2018-12-19 19:51:59 +0800

    Update linux.sh
    minor update

--- linux_shell/linux.sh
+++ linux_shell/linux.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-echo "copy the libmemenv.a and libleveldb.a to leveldb/"
-echo "copy autogen-coin-man.sh to topdir"
-cp -rf ./makefile_liblua ../src/
-cp -rf ./liblua53.a ../src/
-cp -rf ./libmemenv.a ../src/leveldb
-cp -rf ./libleveldb.a ../src/leveldb
-cp -rf ./autogen-coin-man.sh ../
-cp -rf ./genbuild.sh ../share/
+echo 'copy the libmemenv.a and libleveldb.a to leveldb'
+echo 'copy autogen-coin-man.sh to top dir'
+
+cp -f ./genbuild.sh ../share/
+cp -f ./liblua53.a ../src/
+cp -f ./libmemenv.a ../src/leveldb/
+cp -f ./libleveldb.a ../src/leveldb/
+cp -f ./makefile_liblua ../src/
+cp -f ./autogen-coin-man.sh ../
