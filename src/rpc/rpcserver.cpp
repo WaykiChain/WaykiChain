799f5f284463b09da46b0af555aeb9f8cb96e9b3
Richard Chen <richardchen@Richards-MacBook-Pro.local>
2018-12-21 22:38:33 +0800

    use application/json for JSON RPC invocation

--- src/rpc/rpcserver.cpp
+++ src/rpc/rpcserver.cpp
@@ -917,7 +917,7 @@ string HelpExampleCli(string methodname, string args){
 
 string HelpExampleRpc(string methodname, string args){
     return "> curl --user myusername -d '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
-        "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:8332/\n";
+        "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'Content-Type: application/json;' http://127.0.0.1:8332/\n";
 }
 
 const CRPCTable tableRPC;
