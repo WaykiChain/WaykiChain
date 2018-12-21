d51e42e3f026f783beaa4be7890577fae006052c
Richard Chen <richardchen@Richards-MacBook-Pro.local>
2018-12-21 22:34:10 +0800

    use -d or --data for json string instead of --data-binary for a file

--- src/rpc/rpcserver.cpp
+++ src/rpc/rpcserver.cpp
@@ -916,7 +916,7 @@ string HelpExampleCli(string methodname, string args){
 }
 
 string HelpExampleRpc(string methodname, string args){
-    return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
+    return "> curl --user myusername -d '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
         "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:8332/\n";
 }
 
