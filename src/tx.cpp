486a2447370cab677a615f6ad896d412fb4498be
kevin <c.r2009@163.com>
2019-01-07 19:47:46 +0800

    convert tabs to spaces

--- src/tx.cpp
+++ src/tx.cpp
@@ -629,16 +629,16 @@ bool CTransaction::GetAddress(set<CKeyID> &vAddr, CAccountViewCache &view, CScri
     return true;
 }
 string CTransaction::ToString(CAccountViewCache &view) const {
-	string str;
-	string desId;
-	if (desUserId.type() == typeid(CKeyID)) {
-		desId = boost::get<CKeyID>(desUserId).ToString();
-	} else if (desUserId.type() == typeid(CRegID)) {
-		desId = boost::get<CRegID>(desUserId).ToString();
-	}
-	str += strprintf("txType=%s, hash=%s, ver=%d, srcId=%s desId=%s, llValues=%ld, llFees=%ld, vContract=%s, nValidHeight=%d\n",
-	txTypeArray[nTxType], GetHash().ToString().c_str(), nVersion, boost::get<CRegID>(srcRegId).ToString(), desId.c_str(), llValues, llFees, HexStr(vContract).c_str(), nValidHeight);
-	return str;
+    string str;
+    string desId;
+    if (desUserId.type() == typeid(CKeyID)) {
+        desId = boost::get<CKeyID>(desUserId).ToString();
+    } else if (desUserId.type() == typeid(CRegID)) {
+        desId = boost::get<CRegID>(desUserId).ToString();
+    }
+    str += strprintf("txType=%s, hash=%s, ver=%d, srcId=%s desId=%s, llValues=%ld, llFees=%ld, vContract=%s, nValidHeight=%d\n",
+    txTypeArray[nTxType], GetHash().ToString().c_str(), nVersion, boost::get<CRegID>(srcRegId).ToString(), desId.c_str(), llValues, llFees, HexStr(vContract).c_str(), nValidHeight);
+    return str;
 }
 
 Object CTransaction::ToJSON(const CAccountViewCache &AccountView) const{
