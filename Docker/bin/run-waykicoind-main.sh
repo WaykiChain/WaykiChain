bdb3c951476e9754714ccf277fdb5c0966ad6ea5
Richard Chen <richardchen@Richards-MacBook-Pro.local>
2019-01-23 10:15:06 +0800

    improve bin scripts

---
+++ Docker/bin/run-waykicoind-main.sh
@@ -0,0 +1,6 @@
+cd /opt/docker-instances/waykicoind-main \
+&& docker run --name waykicoind-main -p8920:8920 -p 6968:6968 \
+       -v `pwd`/conf/WaykiChain.conf:/root/.WaykiChain/WaykiChain.conf \
+       -v `pwd`/data:/root/.WaykiChain/main \
+       -v `pwd`/bin:/opt/wicc/bin \
+       -d wicc/waykicoind
