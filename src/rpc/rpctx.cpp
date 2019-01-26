98b14c07692d3ea8990bd2ee16646a94b027b28a
kevin <c.r2009@163.com>
2019-01-26 18:11:31 +0800

    adjust getdelegatelist RPC
    Provide a default delegate number in `getdelegatelist` RPC.

--- src/rpc/rpctx.cpp
+++ src/rpc/rpctx.cpp
@@ -384,7 +384,7 @@ Value registeraccounttx(const Array& params, bool fHelp) {
     if (fee < nDefaultFee) {
         char errorMsg[100] = {'\0'};
         sprintf (errorMsg, "input fee smaller than mintxfee: %ld sawi", nDefaultFee);
-        throw JSONRPCError(RPC_INSUFFICIENT_FEE, errorMsg);  
+        throw JSONRPCError(RPC_INSUFFICIENT_FEE, errorMsg);
     }
 
     //get keyid
@@ -3273,24 +3273,25 @@ Value getrawtx(const Array& params, bool fHelp) {
 }
 
 Value getdelegatelist(const Array& params, bool fHelp) {
-    if(fHelp || params.size() != 1)
-    {
+    if (fHelp || params.size() > 1) {
         throw runtime_error(
-                 "getdelegatelist \n"
-                 "\nreturns the specified number delegates by reversed order voting number\n"
-                 "\nArguments:\n"
-                 "\nResult:\n"
-                 "\ndelegates: [\n"
-                  "account: (CAccount),\n"
-                "]\": (Array)\n"
+                "getdelegatelist \n"
+                "\nreturns the specified number delegates by reversed order voting number.\n"
+                "\nArguments:\n"
+                "1. number           (number, optional) the number of the delegates, default to all delegates.\n"
+                "\nResult:\n"
                 "\nExamples:\n"
-                 + HelpExampleCli("getdelegatelist","11")
-                 + HelpExampleRpc("getdelegatelist","11"));
+                + HelpExampleCli("getdelegatelist", "11")
+                + HelpExampleRpc("getdelegatelist", "11"));
     }
-    int nDelegateNum = params[0].get_int();
+
+    int nDelegateNum = (params.size() == 1) ? params[0].get_int() : IniCfg().GetDelegatesCfg();
     if (nDelegateNum < 1 || nDelegateNum > 11) {
-    throw runtime_error("getdelegatelist : param  in [1, 11]!\n");
+        char errorMsg[100] = {'\0'};
+        sprintf(errorMsg, "input delegate number not between 1 and %ld", IniCfg().GetDelegatesCfg());
+        throw JSONRPCError(RPC_INVALID_PARAMS, errorMsg);
     }
+
     int nIndex = 0;
     CRegID redId(0,0);
     vector<unsigned char> vScriptData;
@@ -3300,39 +3301,34 @@ Value getdelegatelist(const Array& params, bool fHelp) {
     Array delegateArray;
     vector<unsigned char> vScriptKey = {'d','e','l','e','g','a','t','e','_'};
     vector<unsigned char> vDelegatePrefix = vScriptKey;
-    while(--nDelegateNum >= 0) {
-       CRegID regId(0,0);
-      if(contractScriptTemp.GetAppData(0, redId, nIndex, vScriptKey, vScriptData)) {
-          nIndex = 1;
-          vector<unsigned char>::iterator iterVotes = find_first_of(vScriptKey.begin(), vScriptKey.end(), vDelegatePrefix.begin(), vDelegatePrefix.end());
-          string strVoltes(iterVotes+9, iterVotes+25);
-          uint64_t llVotes = 0;
-          /*
-          char *stopstring;
-          llVotes = strtoul(strVoltes.c_str(), &stopstring, 16);
-          */
+    while (--nDelegateNum >= 0) {
+        CRegID regId(0, 0);
+        if (contractScriptTemp.GetAppData(0, redId, nIndex, vScriptKey, vScriptData)) {
+            nIndex = 1;
+            vector<unsigned char>::iterator iterVotes = find_first_of(vScriptKey.begin(), vScriptKey.end(), vDelegatePrefix.begin(), vDelegatePrefix.end());
+            string strVoltes(iterVotes+9, iterVotes+25);
+            uint64_t llVotes = 0;
+            stringstream strValue;
+            strValue.flags(ios::hex);
+            strValue << strVoltes;
+            strValue >> llVotes;
 
-          stringstream strValue;
-          strValue.flags(ios::hex);
-          strValue << strVoltes;
-          strValue >> llVotes;
+            vector<unsigned char> vAcctRegId(iterVotes+26, vScriptKey.end());
+            CRegID acctRegId(vAcctRegId);
+            CAccount account;
+            if (!view.GetAccount(acctRegId,account)) {
+                assert(0);
+            }
 
-          vector<unsigned char> vAcctRegId(iterVotes+26, vScriptKey.end());
-          CRegID acctRegId(vAcctRegId);
-          CAccount account;
-          if(!view.GetAccount(acctRegId,account)) {
-              assert(0);
-          }
-          uint64_t maxNum = 0xFFFFFFFFFFFFFFFF;
-          if((maxNum-llVotes) != account.llVotes) {
-              LogPrint("INFO", "llVotes:%lld, account:%s",maxNum-llVotes, account.ToString());
-              assert(0);
-          }
-          delegateArray.push_back(account.ToJsonObj());
-      }
-      else {
-          assert(0);
-      }
+            uint64_t maxNum = 0xFFFFFFFFFFFFFFFF;
+            if ((maxNum - llVotes) != account.llVotes) {
+                LogPrint("INFO", "llVotes:%lld, account:%s",maxNum-llVotes, account.ToString());
+                assert(0);
+            }
+            delegateArray.push_back(account.ToJsonObj());
+        } else {
+            assert(0);
+        }
     }
     obj.push_back(Pair("delegates", delegateArray));
     return obj;
