351a290f155d9c5c34babae75bdd3975bd058223
kevin <c.r2009@163.com>
2019-01-30 14:42:14 +0800

    remove protobuf dependency

--- macosxshell/autogen-coin-man.sh
+++ macosxshell/autogen-coin-man.sh
@@ -1,128 +1,128 @@
 #!/bin/sh
 set -e
 
-if [ $# = 0 ]; then	
-  	echo -e "\033[40;33m"
-		echo Warning:  your did not input assemble model
-		echo "$PWD"
-		echo autogen-coin-man [MODEL NAME]
-		echo
-		echo	EXAMPLE:
-		echo
-		echo	autogen-coin-man ["coin|coin-test|coin-ptest"]
-	echo -e "\033[40;37m"
-		exit 1
+if [ $# = 0 ]; then
+    echo -e "\033[40;33m"
+        echo Warning:  your did not input assemble model
+        echo "$PWD"
+        echo autogen-coin-man [MODEL NAME]
+        echo
+        echo    EXAMPLE:
+        echo
+        echo    autogen-coin-man ["coin|coin-test|coin-ptest"]
+    echo -e "\033[40;37m"
+        exit 1
 elif [ $# = 1 ]; then
-	case $1 in 
-		coin)
-		flag1=--with-daemon
-		;;
-		coin-test)
-		flag1=--enable-tests
-		;;
-		coin-ptest)
-		flag1=--enable-ptests
-		;;
-		*)
-		echo -e "\033[40;32m"
-		echo Warning: error in param!
-		echo -e "\033[40;37m"
-		exit 1
-		;;
-	esac
+    case $1 in
+        coin)
+        flag1=--with-daemon
+        ;;
+        coin-test)
+        flag1=--enable-tests
+        ;;
+        coin-ptest)
+        flag1=--enable-ptests
+        ;;
+        *)
+        echo -e "\033[40;32m"
+        echo Warning: error in param!
+        echo -e "\033[40;37m"
+        exit 1
+        ;;
+    esac
 elif [ $# = 2 ]; then
-	case $1 in 
-		coin)
-		flag1=--with-daemon
-		;;
-		coin-test)
-		flag1=--enable-tests
-		;;
-		coin-ptest)
-		flag1=--enable-ptests
-		;;
-		*)
-		echo -e "\033[40;32m"
-		echo Warning: error in param!
-		echo -e "\033[40;37m"
-		exit 1
-		;;
-	esac
-	case $2 in 
-		coin)
-		flag2=--with-daemon
-		;;
-		coin-test)
-		flag2=--enable-tests
-		;;
-		coin-ptest)
-		flag2=--enable-ptests
-		;;
-		*)
-		echo -e "\033[40;32m"
-		echo Warning: error in param!
-		echo -e "\033[40;37m"
-		exit 1
-		;;
-	esac
+    case $1 in
+        coin)
+        flag1=--with-daemon
+        ;;
+        coin-test)
+        flag1=--enable-tests
+        ;;
+        coin-ptest)
+        flag1=--enable-ptests
+        ;;
+        *)
+        echo -e "\033[40;32m"
+        echo Warning: error in param!
+        echo -e "\033[40;37m"
+        exit 1
+        ;;
+    esac
+    case $2 in
+        coin)
+        flag2=--with-daemon
+        ;;
+        coin-test)
+        flag2=--enable-tests
+        ;;
+        coin-ptest)
+        flag2=--enable-ptests
+        ;;
+        *)
+        echo -e "\033[40;32m"
+        echo Warning: error in param!
+        echo -e "\033[40;37m"
+        exit 1
+        ;;
+    esac
 elif [ $# = 3 ]; then
-	case $1 in 
-		coin)
-		flag1=--with-daemon
-		;;
-		coin-test)
-		flag1=--enable-tests
-		;;
-		coin-ptest)
-		flag1=--enable-ptests
-		;;
-		*)
-		echo -e "\033[40;32m"
-		echo Warning: error in param!
-		echo -e "\033[40;37m"
-		exit 1
-		;;
-	esac
-	case $2 in 
-		coin)
-		flag2=--with-daemon
-		;;
-		coin-test)
-		flag2=--enable-tests
-		;;
-		coin-ptest)
-		flag2=--enable-ptests
-		;;
-		*)
-		echo -e "\033[40;32m"
-		echo Warning: error in param!
-		echo -e "\033[40;37m"
-		exit 1
-		;;
-	esac
-	case $3 in 
-		coin)
-		flag3=--with-daemon
-		;;
-		coin-test)
-		flag3=--enable-tests
-		;;
-		coin-ptest)
-		flag3=--enable-ptests
-		;;
-		*)
-		echo -e "\033[40;32m"
-		echo warming:error para!
-		echo -e "\033[40;37m"
-		exit 1
-		;;
-	esac
+    case $1 in
+        coin)
+        flag1=--with-daemon
+        ;;
+        coin-test)
+        flag1=--enable-tests
+        ;;
+        coin-ptest)
+        flag1=--enable-ptests
+        ;;
+        *)
+        echo -e "\033[40;32m"
+        echo Warning: error in param!
+        echo -e "\033[40;37m"
+        exit 1
+        ;;
+    esac
+    case $2 in
+        coin)
+        flag2=--with-daemon
+        ;;
+        coin-test)
+        flag2=--enable-tests
+        ;;
+        coin-ptest)
+        flag2=--enable-ptests
+        ;;
+        *)
+        echo -e "\033[40;32m"
+        echo Warning: error in param!
+        echo -e "\033[40;37m"
+        exit 1
+        ;;
+    esac
+    case $3 in
+        coin)
+        flag3=--with-daemon
+        ;;
+        coin-test)
+        flag3=--enable-tests
+        ;;
+        coin-ptest)
+        flag3=--enable-ptests
+        ;;
+        *)
+        echo -e "\033[40;32m"
+        echo warming:error para!
+        echo -e "\033[40;37m"
+        exit 1
+        ;;
+    esac
 else
-	echo -e "\033[40;32m"
-	echo Warning:  You input an illegal param value
-   	echo Please ensure the param is of the following values: [coin|coin-test|coin-ptest]
-	echo -e "\033[40;37m" 
-	exit 1
+    echo -e "\033[40;32m"
+    echo Warning:  You input an illegal param value
+    echo Please ensure the param is of the following values: [coin|coin-test|coin-ptest]
+    echo -e "\033[40;37m"
+    exit 1
 fi
 
 srcdir="$(dirname $0)"
