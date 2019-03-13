8e31003569739cee9451dc0f9f62e917cfda08b9
tigerich <tigerich@163.com>
2019-03-13 15:29:37 +0800

    fix genblockforce check issue

--- src/miner.cpp
+++ src/miner.cpp
@@ -641,12 +641,12 @@ void static CoinMiner(CWallet *pwallet, int targetHeight) {
                 // Busy-wait for the network to come online so we don't waste time mining
                 // on an obsolete chain. In regtest mode we expect to fly solo.
                 while ( vNodes.empty() ||
-                    (   !SysCfg().GetBoolArg("-genblockforce", false) &&
-                        chainActive.Tip() &&
+                        (chainActive.Tip() &&
                         chainActive.Tip()->nHeight > 1 &&
-                        GetAdjustedTime() - chainActive.Tip()->nTime > 60 * 60
-                    ))
+                        GetAdjustedTime() - chainActive.Tip()->nTime > 60 * 60 &&
+                        !SysCfg().GetBoolArg("-genblockforce", false)) )
                 {
+                    LogPrint("INFO", "sleep 1");
                     MilliSleep(1000);
                 }
             }
