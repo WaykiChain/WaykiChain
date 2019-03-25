5fc9d55f2e5c8ef1c2a1ef38941304436e3496ee
chenxinhua2018 <15220088021@163.com>
2019-03-25 17:02:48 +0800

    set the "CFLAGS=-fPIC"  for liblua53.a only

--- linuxshell/autogen-coin-man.sh
+++ linuxshell/autogen-coin-man.sh
@@ -46,4 +46,4 @@ done
 srcdir="$(dirname $0)"
 cd "$srcdir"
 autoreconf --install --force
-CFLAGS="-fPIC" ./configure --disable-upnp-default --without-gui --with-incompatible-bdb $Modules
+./configure --disable-upnp-default --without-gui --with-incompatible-bdb $Modules
