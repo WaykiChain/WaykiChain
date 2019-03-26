6aa8fbcc055dd9933f393ce090305a1cdb592754
Kevin <c.r2009@163.com>
2019-03-26 11:37:51 +0800

    compile leveldb from src

--- linuxshell/linux.sh
+++ linuxshell/linux.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-echo 'copy the libmemenv.a and libleveldb.a to leveldb'
 echo 'copy autogen-coin-man.sh to top dir'
-
 cp -f ./genbuild.sh ../share/
-cp -f ./libmemenv.a ../src/leveldb/
-cp -f ./libleveldb.a ../src/leveldb/
 cp -f ./autogen-coin-man.sh ../
+
+chmod +x ../src/leveldb/build_detect_platform
