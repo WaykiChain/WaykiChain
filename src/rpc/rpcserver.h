75b29f8cfae537cfb8417984165f1cb35624d6c8
Kevin <c.r2009@163.com>
2019-03-27 18:44:35 +0800

    remove unused code

--- src/rpc/rpcserver.h
+++ src/rpc/rpcserver.h
@@ -153,7 +153,6 @@ extern json_spirit::Value getnetworkinfo(const json_spirit::Array& params, bool 
 extern json_spirit::Value signmessage(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value notionalpoolingbalance(const json_spirit::Array& params, bool fHelp);
 // extern json_spirit::Value (const json_spirit::Array& params, bool fHelp);
-extern json_spirit::Value notionalpoolingasset(const json_spirit:: Array& params, bool fHelp);
 extern json_spirit::Value getassets(const json_spirit:: Array& params, bool fHelp);
 extern json_spirit::Value sendtoaddress(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value sendtoaddresswithfee(const json_spirit::Array& params, bool fHelp);
