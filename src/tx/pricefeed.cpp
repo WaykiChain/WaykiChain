884f0a82620d86175d354c5ff49d63c0f3946cab
tigerich <tigerich@163.com>
2019-05-07 14:09:48 +0800

    fix pricefeed

--- src/tx/pricefeed.cpp
+++ src/tx/pricefeed.cpp
@@ -70,7 +70,7 @@ bool CPriceFeedTx::UndoExecuteTx(int nIndex, CAccountViewCache &view, CValidatio
 
 Object CPriceFeedTx::ToJson(const CAccountViewCache &AccountView) const {
   //TODO
-  return CNullID();
+  return Object();
 }
 
 bool CPriceFeedTx::GetAddress(set<CKeyID> &vAddr, CAccountViewCache &view, CScriptDBViewCache &scriptDB) {
