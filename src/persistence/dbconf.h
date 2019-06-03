29d7cc0fac2c34046467f7db71eac247478a397a
chenxinhua2018 <15220088021@163.com>
2019-06-03 17:33:34 +0800

    Improve PREFIX_COUNT of PrefixType

--- src/persistence/dbconf.h
+++ src/persistence/dbconf.h
@@ -17,43 +17,43 @@
  */
 namespace dbk {
 
-    #define EACH_ENUM_DEFINE_TYPE(enumType, enumName, description) enumType,
-    #define EACH_ENUM_DEFINE_NAMES(enumType, enumName, description) enumName,
-    #define EACH_ENUM_DEFINE_NAME(enumType, enumName, description) \
-        static const std::string PREFIX_##enumName = enumName;
+    #define EACH_ENUM_DEFINE_TYPE(enumType, enumName) enumType,
+    #define EACH_ENUM_DEFINE_NAMES(enumType, enumName) enumName,
 
-        //                 type                name(prefix)       description
-        //               ----------           ------------   -----------------------------
+    //                 type                name(prefix)       description
+    //               ----------           ------------   -----------------------------
     #define DBK_PREFIX_LIST(EACH_ENUM_DEFINE) \
-        EACH_ENUM_DEFINE( EMPTY,              "",         "empty prefix" ) \
-        EACH_ENUM_DEFINE( REINDEX,            "ridx",     "[prefix] --> $Reindex = 1 | 0" ) \
-        EACH_ENUM_DEFINE( FLAG,               "flag",     "[prefix] --> $Flag = 1 | 0" ) \
-        EACH_ENUM_DEFINE( VOTE,               "vote",     "vote{(uint64t)MAX - $votedBcoins}_{$RegId} --> 1" ) \
-        EACH_ENUM_DEFINE( KEYID_TXID,         "idtx",     "idtx{$KeyId}{$Height}{$Index} --> $txid" ) \
-        EACH_ENUM_DEFINE( TXID_KEYIDS,        "tids",     "tids{$txid} --> {$KeyId1, $KeyId2, ...}" ) \
-        EACH_ENUM_DEFINE( TXID_DISKINDEX,     "tidx",     "tidx{$txid} --> $DiskTxPos" ) \
-        EACH_ENUM_DEFINE( REGID_KEYID,        "rkey",     "rkey{$RegID} --> $KeyId" ) \
-        EACH_ENUM_DEFINE( NICKID_KEYID,       "nkey",     "nkey{$NickID} --> $KeyId" ) \
-        EACH_ENUM_DEFINE( KEYID_ACCOUNT,      "idac",     "idac{$KeyID} --> $CAccount" ) \
-        EACH_ENUM_DEFINE( BEST_BLOCKHASH,     "bbkh",     "[prefix] --> $BestBlockHash" ) \
-        EACH_ENUM_DEFINE( BLOCK_INDEX,        "bidx",     "pbfl --> $nFile" ) \
-        EACH_ENUM_DEFINE( BLOCKFILE_NUM_INFO, "bfni",     "BlockFileNum --> $BlockFileInfo" ) \
-        EACH_ENUM_DEFINE( LAST_BLOCKFILE,     "ltbf",     "[prefix] --> $LastBlockFile" ) \
-        EACH_ENUM_DEFINE( CONTRACT_COUNT,     "cnum",     "cnum{$ContractRegId} --> $total_num_of_cntracts" ) \
-        EACH_ENUM_DEFINE( CONTRACT_DEF,       "cdef",     "cdef{$ContractRegId} --> $ContractContent" ) \
-        EACH_ENUM_DEFINE( CONTRACT_DATA,      "cdat",     "cdat{$RegId}_{$DataKey} --> $Data" ) \
-        EACH_ENUM_DEFINE( CONTRACT_TX_OUT,    "cout",     "cout{txid} --> $VmOperateOutput" ) \
-        EACH_ENUM_DEFINE( CONTRACT_ITEM_NUM,  "citn",     "citn{ContractRegId} --> $total_num_of_contract_i" ) \
-        EACH_ENUM_DEFINE( STAKE_FCOIN,        "fcoin",    "fcoin{(uint64t)MAX - stakedFcoins}_{RegId} --> 1" ) \
-        EACH_ENUM_DEFINE( CDP,                "cdp",      "cdp{$RegID} --> blockHeight,mintedScoins")
+        EACH_ENUM_DEFINE( EMPTY,              "")         /* empty prefix ) */ \
+        EACH_ENUM_DEFINE( REINDEX,            "ridx")     /* [prefix] --> $Reindex = 1 | 0 */ \
+        EACH_ENUM_DEFINE( FLAG,               "flag")     /* [prefix] --> $Flag = 1 | 0 */ \
+        EACH_ENUM_DEFINE( VOTE,               "vote")     /* "vote{(uint64t)MAX - $votedBcoins}_{$RegId} --> 1 */ \
+        EACH_ENUM_DEFINE( KEYID_TXID,         "idtx")     /* idtx{$KeyId}{$Height}{$Index} --> $txid */ \
+        EACH_ENUM_DEFINE( TXID_KEYIDS,        "tids")     /* tids{$txid} --> {$KeyId1, $KeyId2, ...}*/ \
+        EACH_ENUM_DEFINE( TXID_DISKINDEX,     "tidx")     /* tidx{$txid} --> $DiskTxPos */ \
+        EACH_ENUM_DEFINE( REGID_KEYID,        "rkey")     /* rkey{$RegID} --> $KeyId */ \
+        EACH_ENUM_DEFINE( NICKID_KEYID,       "nkey")     /* nkey{$NickID} --> $KeyId */ \
+        EACH_ENUM_DEFINE( KEYID_ACCOUNT,      "idac")     /* idac{$KeyID} --> $CAccount */ \
+        EACH_ENUM_DEFINE( BEST_BLOCKHASH,     "bbkh")     /* [prefix] --> $BestBlockHash */ \
+        EACH_ENUM_DEFINE( BLOCK_INDEX,        "bidx")     /* pbfl --> $nFile */ \
+        EACH_ENUM_DEFINE( BLOCKFILE_NUM_INFO, "bfni")     /* BlockFileNum --> $BlockFileInfo */ \
+        EACH_ENUM_DEFINE( LAST_BLOCKFILE,     "ltbf")     /* [prefix] --> $LastBlockFile */ \
+        EACH_ENUM_DEFINE( CONTRACT_COUNT,     "cnum")     /* cnum{$ContractRegId} --> $total_num_of_cntracts */ \
+        EACH_ENUM_DEFINE( CONTRACT_DEF,       "cdef")     /* cdef{$ContractRegId} --> $ContractContent */ \
+        EACH_ENUM_DEFINE( CONTRACT_DATA,      "cdat")     /* cdat{$RegId}_{$DataKey} --> $Data */ \
+        EACH_ENUM_DEFINE( CONTRACT_TX_OUT,    "cout")     /* cout{txid} --> $VmOperateOutput */ \
+        EACH_ENUM_DEFINE( CONTRACT_ITEM_NUM,  "citn")     /* citn{ContractRegId} --> $total_num_of_contract_i */ \
+        EACH_ENUM_DEFINE( STAKE_FCOIN,        "fcoin")    /* fcoin{(uint64t)MAX - stakedFcoins}_{RegId} --> 1 */ \
+        EACH_ENUM_DEFINE( CDP,                "cdp")      /* cdp{$RegID} --> blockHeight,mintedScoins */ \
+        \
+        /* Add new Enum elements above, PREFIX_COUNT Must be the last one */ \
+        EACH_ENUM_DEFINE( PREFIX_COUNT,        "")       /* enum count, must be the last one */
+
 
     enum PrefixType {
         DBK_PREFIX_LIST(EACH_ENUM_DEFINE_TYPE)
     };
 
-    static const int PREFIX_COUNT = PrefixType::STAKE_FCOIN + 1;
-
-    static const std::string gPrefixNames[PREFIX_COUNT] = {
+    static const std::string gPrefixNames[PREFIX_COUNT + 1] = {
         DBK_PREFIX_LIST(EACH_ENUM_DEFINE_NAMES)
     };
 
