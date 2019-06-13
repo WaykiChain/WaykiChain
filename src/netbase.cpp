10358a7974e73fe95479ff62f71458e97b537d12
Kevin <c.r2009@163.com>
2019-06-13 20:16:01 +0800

    create fund coin genesis block

--- src/netbase.cpp
+++ src/netbase.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The WaykiChain developers
-// Copyright (c) 2016 The Coin developers
+// Copyright (c) 2017-2019 The WaykiChain Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
