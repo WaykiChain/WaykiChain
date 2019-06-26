8bb54076ed1432df13cf9f8032ae8cb62d8fd8ee
chenxinhua2018 <15220088021@163.com>
2019-06-26 18:10:58 +0800

    Add  dex cancel order tx

--- src/tx/dextx.h
+++ src/tx/dextx.h
@@ -187,7 +187,7 @@ public:
         return sigHash;
     }
 
-    virtual std::shared_ptr<CBaseTx> GetNewInstance() { return std::make_shared<CDEXBuyLimitOrderTx>(this); }
+    virtual std::shared_ptr<CBaseTx> GetNewInstance() { return std::make_shared<CDEXBuyMarketOrderTx>(this); }
     virtual double GetPriority() const { return 10000.0f; } // Top priority
     virtual string ToString(CAccountCache &view); //logging usage
     virtual Object ToJson(const CAccountCache &view) const; //json-rpc usage
@@ -203,9 +203,7 @@ public:
     uint64_t coinAmount;   //!< amount of target coin to spend for buying asset
 };
 
-
 class CDEXSellMarketOrderTx : public CDEXOrderBaseTx {
-
 public:
     CDEXSellMarketOrderTx() : CDEXOrderBaseTx(DEX_BUY_ORDER_TX) {}
 
@@ -245,7 +243,7 @@ public:
         return sigHash;
     }
 
-    virtual std::shared_ptr<CBaseTx> GetNewInstance() { return std::make_shared<CDEXBuyLimitOrderTx>(this); }
+    virtual std::shared_ptr<CBaseTx> GetNewInstance() { return std::make_shared<CDEXSellMarketOrderTx>(this); }
     virtual double GetPriority() const { return 10000.0f; } // Top priority
     virtual string ToString(CAccountCache &view); //logging usage
     virtual Object ToJson(const CAccountCache &view) const; //json-rpc usage
@@ -259,6 +257,55 @@ public:
     CoinType coinType;      //!< coin type (wusd) to buy asset
     CoinType assetType;     //!< asset type
     uint64_t assetAmount;   //!< amount of target asset to buy
+};
+
+class CDEXCancelOrderTx : public CBaseTx {
+public:
+    CDEXCancelOrderTx() : CBaseTx(DEX_BUY_ORDER_TX) {}
+
+    CDEXCancelOrderTx(const CBaseTx *pBaseTx): CBaseTx(DEX_BUY_ORDER_TX) {
+        assert(DEX_BUY_ORDER_TX == pBaseTx->nTxType);
+        *this = *(CDEXCancelOrderTx *)pBaseTx;
+    }
+
+    CDEXCancelOrderTx(const CUserID &txUidIn, int validHeightIn, uint64_t feesIn,
+                         uint256 orderIdIn)
+        : CBaseTx(DEX_BUY_ORDER_TX, txUidIn, validHeightIn, feesIn),
+          orderId(orderIdIn){}
+
+    ~CDEXCancelOrderTx() {}
+
+    IMPLEMENT_SERIALIZE(
+        READWRITE(VARINT(this->nVersion));
+        nVersion = this->nVersion;
+        READWRITE(VARINT(nValidHeight));
+        READWRITE(txUid);
+
+        READWRITE(orderId);
+    )
+
+    uint256 ComputeSignatureHash(bool recalculate = false) const {
+        if (recalculate || sigHash.IsNull()) {
+            CHashWriter ss(SER_GETHASH, 0);
+            ss  << VARINT(nVersion) << uint8_t(nTxType) << VARINT(nValidHeight) << txUid
+                << orderId;
+            sigHash = ss.GetHash();
+        }
+
+        return sigHash;
+    }
+
+    virtual std::shared_ptr<CBaseTx> GetNewInstance() { return std::make_shared<CDEXCancelOrderTx>(this); }
+    virtual double GetPriority() const { return 10000.0f; } // Top priority
+    virtual string ToString(CAccountCache &view); //logging usage
+    virtual Object ToJson(const CAccountCache &view) const; //json-rpc usage
+    virtual bool GetInvolvedKeyIds(CCacheWrapper &cw, set<CKeyID> &keyIds);
+
+    virtual bool CheckTx(int nHeight, CCacheWrapper &cw, CValidationState &state);
+    virtual bool ExecuteTx(int nHeight, int nIndex, CCacheWrapper &cw, CValidationState &state);
+    virtual bool UndoExecuteTx(int nHeight, int nIndex, CCacheWrapper &cw, CValidationState &state);
+public:
+    uint256  orderId;       //!< id of oder need to be canceled.
 };
 
 struct DEXDealItem  {
