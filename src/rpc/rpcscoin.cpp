0a0b12b69cde2725a106e27192b41b7b2e70c6c0
tigerich <tigerich@163.com>
2019-07-03 16:46:57 +0800

    add submitpricefeedtx

--- src/rpc/rpcscoin.cpp
+++ src/rpc/rpcscoin.cpp
@@ -17,31 +17,60 @@
 Value submitpricefeedtx(const Array& params, bool fHelp) {
     if (fHelp || params.size() < 2 || params.size() > 4) {
         throw runtime_error(
-            "submitpricefeedtx \"{addr}\" \"script_path\"\n"
+            "submitpricefeedtx \"$pricefeeds\" \n"
             "\nsubmit a price feed tx.\n"
             "\nthe execution include registercontracttx and callcontracttx.\n"
             "\nArguments:\n"
-            "1.\"addr\": (string required) contract owner address from this wallet\n"
-            "2.\"script_path\": (string required), the file path of the app script\n"
-            "3.\"arguments\": (string, optional) contract method invoke content (Hex encode required)\n"
-            "4.\"fee\": (numeric, optional) fee pay for miner, default is 110010000\n"
-            "\nResult vm execute detail\n"
+            "1. \"pricefeeds\"    (string, required) A json array of pricefeeds\n"
+            " [\n"
+            "   {\n"
+            "      \"coin\": \"WICC|WGRT\", (string, required) The coin type\n"
+            "      \"currency\": \"USD|RMB\" (string, required) The currency type\n"
+            "      \"price\": (number, required) The price (boosted by 10^) \n"
+            "   }\n"
+            "       ,...\n"
+            " ]\n"
+            "2.\"fee\": (numeric, optional) fee pay for miner, default is 10000\n"
+            "\nResult pricefeed tx result\n"
             "\nResult:\n"
             "\nExamples:\n"
-            + HelpExampleCli("vmexecutescript","\"WiZx6rrsBn9sHjwpvdwtMNNX2o31s3DEHH\" \"/tmp/lua/script.lua\"\n")
+            + HelpExampleCli("submitpricefeedtx", 'WiZx6rrsBn9sHjwpvdwtMNNX2o31s3DEHH' \"[{\"coin\", WICC, \"currency\": USD, \"price\": 0.28}]\"\n")
             + "\nAs json rpc call\n"
-            + HelpExampleRpc("vmexecutescript", "\"WiZx6rrsBn9sHjwpvdwtMNNX2o31s3DEHH\" \"/tmp/lua/script.lua\"\n"));
+            + HelpExampleRpc("submitpricefeedtx","\"WiZx6rrsBn9sHjwpvdwtMNNX2o31s3DEHH\" \"[{\"coin\", WICC, \"currency\": USD, \"price\": 0.28}]\"\n"));
     }
 }
 
-Value submitstakefcointx(const Array& params, bool fHelp);
-Value submitdexbuyordertx(const Array& params, bool fHelp);
-Value submitdexsellordertx(const Array& params, bool fHelp);
-Value submitdexcancelordertx(const Array& params, bool fHelp);
-Value submitdexsettletx(const Array& params, bool fHelp);
-Value submitstakecdptx(const Array& params, bool fHelp);
-Value submitredeemcdptx(const Array& params, bool fHelp);
-Value submitliquidatecdptx(const Array& params, bool fHelp);
-Value getmedianprice(const Array& params, bool fHelp);
-Value listcdps(const Array& params, bool fHelp);
-Value listcdpstoliquidate(const Array& params, bool fHelp);
+Value submitstakefcointx(const Array& params, bool fHelp) {
+
+    return Object();
+}
+Value submitdexbuyordertx(const Array& params, bool fHelp) {
+    return Object();
+}
+Value submitdexsellordertx(const Array& params, bool fHelp) {
+    return Object();
+}
+Value submitdexcancelordertx(const Array& params, bool fHelp) {
+    return Object();
+}
+Value submitdexsettletx(const Array& params, bool fHelp) {
+    return Object();
+}
+Value submitstakecdptx(const Array& params, bool fHelp) {
+    return Object();
+}
+Value submitredeemcdptx(const Array& params, bool fHelp) {
+    return Object();
+}
+Value submitliquidatecdptx(const Array& params, bool fHelp) {
+    return Object();
+}
+Value getmedianprice(const Array& params, bool fHelp) {
+    return Object();
+}
+Value listcdps(const Array& params, bool fHelp) {
+    return Object();
+}
+Value listcdpstoliquidate(const Array& params, bool fHelp) {
+    return Object();
+}
