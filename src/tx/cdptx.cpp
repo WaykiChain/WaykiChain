56ccaef945ee1c88f5a177fbe39e63920a4a37b1
Kevin <c.r2009@163.com>
2019-07-25 18:05:28 +0800

    fix compilation issue

--- src/tx/cdptx.cpp
+++ src/tx/cdptx.cpp
@@ -192,11 +192,8 @@ bool CCDPStakeTx::ExecuteTx(int32_t height, int32_t index, CCacheWrapper &cw, CV
 
     vector<CReceipt> receipts;
     CUserID nullUid;
-<<<<<<< HEAD
+
     CReceipt receipt(nTxType, nullUid, txUid, WUSD, scoins_to_mint);
-=======
-    CReceipt receipt(nTxType, nullUid, txUid, SYMB::WUSD, scoinsToMint);
->>>>>>> 5c581fb1f7a8f56c883815fd97a9c5c78d1d653c
     receipts.push_back(receipt);
     cw.txReceiptCache.SetTxReceipts(GetHash(), receipts);
 
