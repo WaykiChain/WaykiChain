9b9226c7b13789140ae1facd6b26513ecf05a47a
chenxinhua2018 <15220088021@163.com>
2019-07-26 14:27:40 +0800

    Fix the wrong prefix for regId2CdpCache

--- src/persistence/cdpdb.h
+++ src/persistence/cdpdb.h
@@ -94,7 +94,7 @@ private:
     // cdp$CTxID -> CUserCDP
     CCompositeKVCache< dbk::CDP,         uint256,       CUserCDP >       cdpCache;
     // rcdp${CRegID} -> set<CTxID>
-    CCompositeKVCache< dbk::REGID_KEYID, string,        set<uint256>>    regId2CdpCache;
+    CCompositeKVCache< dbk::REGID_CDP, string,        set<uint256>>    regId2CdpCache;
 
 public:
     // Memory only cache
