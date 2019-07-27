4fa91002c5bb0919223135ae892b0dce655685bb
tigerich <tigerich@163.com>
2019-07-27 17:40:29 +0800

    fix compile issue

--- src/config/txbase.h
+++ src/config/txbase.h
@@ -73,7 +73,7 @@ struct TxTypeHash {
  * Fees are boosted by 10^8
  */
 static const unordered_map<TxType, std::tuple<string, uint64_t, uint64_t, uint64_t, uint64_t>, TxTypeHash> kTxFeeTable = {
-{ NULL_TX,                  std::make_tuple("NULL_TX",                  0,          0,         0,          0            ) }
+{ NULL_TX,                  std::make_tuple("NULL_TX",                  0,          0,         0,          0            ) },
 
 { BLOCK_REWARD_TX,          std::make_tuple("BLOCK_REWARD_TX",          0,          0,         0,          0            ) },
 { ACCOUNT_REGISTER_TX,      std::make_tuple("ACCOUNT_REGISTER_TX",      0,          10000,     10000,      10000        ) }, //0.0001 WICC, optional
