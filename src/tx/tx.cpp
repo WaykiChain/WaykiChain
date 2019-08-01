85d35c15822b433a3f0a9911e41ebf99546f8828
Kevin <c.r2009@163.com>
2019-08-01 10:58:19 +0800

    fix: check fee type

--- src/tx/tx.cpp
+++ src/tx/tx.cpp
@@ -85,7 +85,8 @@ uint32_t CBaseTx::GetFuelRate(CContractDBCache &scriptDB) {
 }
 
 bool CBaseTx::CheckTxFeeSufficient(const TokenSymbol &feeSymbol, const uint64_t llFees, const int32_t height) const {
-    assert(feeSymbol == SYMB::WICC || feeSymbol == SYMB::WUSD);
+    if (feeSymbol == SYMB::WICC || feeSymbol == SYMB::WUSD)
+        return false;
 
     const auto &iter = kTxFeeTable.find(nTxType);
 
