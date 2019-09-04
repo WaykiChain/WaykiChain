1488a88b913083cac6500a35b3d7c5eb3011a98c
Kevin <c.r2009@163.com>
2019-09-04 14:35:04 +0800

    redefine rpc error code

--- src/rpc/core/rpcprotocol.h
+++ src/rpc/core/rpcprotocol.h
@@ -34,63 +34,61 @@ enum HTTPStatusCode
 };
 
 // Coin RPC error codes
-enum RPCErrorCode
-{
+enum RPCErrorCode {
     //! Standard JSON-RPC 2.0 errors
     // RPC_INVALID_REQUEST is internally mapped to HTTP_BAD_REQUEST (400).
     // It should not be used for application-layer errors.
-    RPC_INVALID_REQUEST  = -32600,
+    RPC_INVALID_REQUEST = -32600,
     // RPC_METHOD_NOT_FOUND is internally mapped to HTTP_NOT_FOUND (404).
     // It should not be used for application-layer errors.
     RPC_METHOD_NOT_FOUND = -32601,
     RPC_INVALID_PARAMS   = -32602,
     // RPC_INTERNAL_ERROR should only be used for genuine errors in coind
     // (for example datadir corruption).
-    RPC_INTERNAL_ERROR   = -32603,
-    RPC_PARSE_ERROR      = -32700,
+    RPC_INTERNAL_ERROR = -32603,
+    RPC_PARSE_ERROR    = -32700,
 
     // General application defined errors
-    RPC_MISC_ERROR                  = -1,  // exception thrown in command handling
-    RPC_FORBIDDEN_BY_SAFE_MODE      = -2,  // Server is in safe mode, and command is not allowed in safe mode
-    RPC_TYPE_ERROR                  = -3,  // Unexpected type was passed as parameter
-    RPC_INSUFFICIENT_FEE            = -4,  // Fee less than Default Transaction Fee (E.g. 10000)
-    RPC_INVALID_ADDRESS_OR_KEY      = -5,  // Invalid address or key
-    RPC_OUT_OF_MEMORY               = -7,  // Ran out of memory during operation
-    RPC_INVALID_PARAMETER           = -8,  // Invalid, missing or duplicate parameter
-    RPC_DATABASE_ERROR              = -20, // Database error
-    RPC_DESERIALIZATION_ERROR       = -22, // Error parsing or validating structure in raw format
-    RPC_TRANSACTION_ERROR           = -25, // General error during transaction submission
-    RPC_TRANSACTION_REJECTED        = -26, // Transaction was rejected by network rules
-    RPC_TRANSACTION_ALREADY_IN_CHAIN= -27, // Transaction already in chain
+    RPC_MISC_ERROR                   = -1,   // exception thrown in command handling
+    RPC_FORBIDDEN_BY_SAFE_MODE       = -2,   // Server is in safe mode, and command is not allowed in safe mode
+    RPC_TYPE_ERROR                   = -3,   // Unexpected type was passed as parameter
+    RPC_INSUFFICIENT_FEE             = -4,   // Fee less than Default Transaction Fee (E.g. 10000)
+    RPC_INVALID_ADDRESS_OR_KEY       = -5,   // Invalid address or key
+    RPC_OUT_OF_MEMORY                = -7,   // Ran out of memory during operation
+    RPC_INVALID_PARAMETER            = -8,   // Invalid, missing or duplicate parameter
+    RPC_DATABASE_ERROR               = -9,   // Database error
+    RPC_DESERIALIZATION_ERROR        = -10,  // Error parsing or validating structure in raw format
+    RPC_TRANSACTION_ERROR            = -11,  // General error during transaction submission
+    RPC_TRANSACTION_REJECTED         = -12,  // Transaction was rejected by network rules
+    RPC_TRANSACTION_ALREADY_IN_CHAIN = -13,  // Transaction already in chain
 
     // P2P client errors
-    RPC_CLIENT_NOT_CONNECTED        = -9,  // Coin is not connected
-    RPC_CLIENT_IN_INITIAL_DOWNLOAD  = -10, // Still downloading initial blocks
-    RPC_CLIENT_NODE_ALREADY_ADDED   = -23, // Node is already added
-    RPC_CLIENT_NODE_NOT_ADDED       = -24, // Node has not been added before
-    RPC_CLIENT_IS_LOCALHOST_ERROR   = -25, // Node can't be a local host one
+    RPC_CLIENT_NOT_CONNECTED       = -1001,  // Coin is not connected
+    RPC_CLIENT_IN_INITIAL_DOWNLOAD = -1002,  // Still downloading initial blocks
+    RPC_CLIENT_NODE_ALREADY_ADDED  = -1003,  // Node is already added
+    RPC_CLIENT_NODE_NOT_ADDED      = -1004,  // Node has not been added before
+    RPC_CLIENT_IS_LOCALHOST_ERROR  = -1005,  // Node can't be a local host one
 
     // Wallet errors
-    RPC_WALLET_ERROR                = -4,  // Unspecified problem with wallet (key not found etc.)
-    RPC_WALLET_INSUFFICIENT_FUNDS   = -6,  // Not enough funds in wallet or account
-    RPC_WALLET_FILEPATH_INVALID     = -7,  // wallet dump or import file path error
-    RPC_WALLET_INVALID_ACCOUNT_NAME = -11, // Invalid account name
-    RPC_WALLET_KEYPOOL_RAN_OUT      = -12, // Keypool ran out, call keypoolrefill first
-    RPC_WALLET_UNLOCK_NEEDED        = -13, // Enter the wallet passphrase with walletpassphrase first
-    RPC_WALLET_PASSPHRASE_INCORRECT = -14, // The wallet passphrase entered was incorrect
-    RPC_WALLET_WRONG_ENC_STATE      = -15, // Command given in wrong wallet encryption state (encrypting an encrypted wallet etc.)
-    RPC_WALLET_ENCRYPTION_FAILED    = -16, // Failed to encrypt the wallet
-    RPC_WALLET_ALREADY_UNLOCKED     = -17, // Wallet is already unlocked
+    RPC_WALLET_ERROR                = -2001,  // Unspecified problem with wallet (key not found etc.)
+    RPC_WALLET_INSUFFICIENT_FUNDS   = -2002,  // Not enough funds in wallet or account
+    RPC_WALLET_FILEPATH_INVALID     = -2003,  // wallet dump or import file path error
+    RPC_WALLET_INVALID_ACCOUNT_NAME = -2004,  // Invalid account name
+    RPC_WALLET_KEYPOOL_RAN_OUT      = -2005,  // Keypool ran out, call keypoolrefill first
+    RPC_WALLET_UNLOCK_NEEDED        = -2006,  // Enter the wallet passphrase with walletpassphrase first
+    RPC_WALLET_PASSPHRASE_INCORRECT = -2007,  // The wallet passphrase entered was incorrect
+    RPC_WALLET_WRONG_ENC_STATE      = -2008,  // Command given in wrong wallet encryption state (encrypting an encrypted wallet etc.)
+    RPC_WALLET_ENCRYPTION_FAILED    = -2009,  // Failed to encrypt the wallet
+    RPC_WALLET_ALREADY_UNLOCKED     = -2010,  // Wallet is already unlocked
 
-    RPC_SCRIPT_FILEPATH_INVALID     = -18, // Register contract script file not inside /tmp/lua dir
-    RPC_SCRIPT_FILEPATH_NOT_EXIST   = -19, // Register contract script file not exist
+    RPC_SCRIPT_FILEPATH_INVALID   = -3001,  // Register contract script file not inside /tmp/lua dir
+    RPC_SCRIPT_FILEPATH_NOT_EXIST = -3002,  // Register contract script file not exist
+    RPC_MEMO_SIZE_TOO_LONG        = -3003,  // memo size too long
 
-
-    // dex errors
-    RPC_COIN_TYPE_INVALID       = -100, // coin type invalid
-    RPC_ASSET_TYPE_INVALID      = -101, // asset type invalid
-    RPC_DEX_ORDER_INACTIVE      = -102, // the order is inactive or not exists
-    RPC_MEMO_SIZE_TOO_LONG      = -103
+    // DEX errors
+    RPC_COIN_TYPE_INVALID  = -4001,  // coin type invalid
+    RPC_ASSET_TYPE_INVALID = -4002,  // asset type invalid
+    RPC_DEX_ORDER_INACTIVE = -4003,  // the order is inactive or not exists
 };
 
 //
