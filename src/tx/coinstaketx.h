b410326470cffd9c3862ad6b25c5293692b88b9f
tigerich <tigerich@163.com>
2019-09-13 09:46:19 +0800

    use UCOIN_STAKE_H as head marcro

--- src/tx/coinstaketx.h
+++ src/tx/coinstaketx.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef FCOIN_STAKE_H
-#define FCOIN_STAKE_H
+#ifndef UCOIN_STAKE_H
+#define UCOIN_STAKE_H
 
 #include "tx.h"
 
