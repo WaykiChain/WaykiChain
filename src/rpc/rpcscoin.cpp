ade8883f7a469a0a7dc9fde7c392f3995ee4847c
richardchen <tigerich@163.com>
2019-09-18 15:38:19 +0800

    fix zero divide  error for global collateral ratio

--- src/rpc/rpcscoin.cpp
+++ src/rpc/rpcscoin.cpp
@@ -351,11 +351,12 @@ Value getscoininfo(const Array& params, bool fHelp){
     obj.push_back(Pair("global_collateral_ceiling",             globalCollateralCeiling * COIN));
     obj.push_back(Pair("global_collateral_ceiling_reached",     global_collateral_ceiling_reached));
 
-    obj.push_back(Pair("global_collateral_ratio_floor",         strprintf("%.4f%%", (double)globalCollateralRatioFloor / RATIO_BOOST * 100)));
-    obj.push_back(Pair("global_collateral_ratio",               strprintf("%.4f%%", (double)globalCollateralRatio / RATIO_BOOST * 100)));
+    string gcr = globalOwedScoins == 0 ? "INF" ? strprintf("%.2f%%", (double)globalCollateralRatio / RATIO_BOOST * 100));
+    obj.push_back(Pair("global_collateral_ratio",               gcr);
+    obj.push_back(Pair("global_collateral_ratio_floor",         strprintf("%.2f%%", (double)globalCollateralRatioFloor / RATIO_BOOST * 100)));
     obj.push_back(Pair("global_collateral_ratio_floor_reached", globalCollateralRatioFloorReached));
 
-    obj.push_back(Pair("force_liquidate_ratio",                 strprintf("%.4f%%", (double)forceLiquidateRatio / RATIO_BOOST * 100)));
+    obj.push_back(Pair("force_liquidate_ratio",                 strprintf("%.2f%%", (double)forceLiquidateRatio / RATIO_BOOST * 100)));
     obj.push_back(Pair("force_liquidate_cdp_amount",            forceLiquidateCdps.size()));
 
     return obj;
