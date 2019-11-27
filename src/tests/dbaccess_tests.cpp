343c62a57f97c913854f0795092237af3774011f
chenxinhua2018 <15220088021@163.com>
2019-11-27 09:58:19 +0800

    fix compile error of unit tests

--- src/tests/dbaccess_tests.cpp
+++ src/tests/dbaccess_tests.cpp
@@ -14,13 +14,37 @@ using namespace std;
 
 static const CRegID id; // to fix the link error: undefined reference to `CRegID ...
 
-BOOST_AUTO_TEST_SUITE(dbaccess_tests)
+struct FDBAccessTests {
+    FDBAccessTests() {
+        BOOST_TEST_MESSAGE( "setup FDBAccessTests" );
+        root_dir = "/tmp/coind_unit_test";
+        if (boost::filesystem::exists(root_dir))
+            BOOST_CHECK(boost::filesystem::is_directory(root_dir));
+        else
+            BOOST_CHECK_NO_THROW(boost::filesystem::create_directory(root_dir));
+
+        db_dir = root_dir / "dbaccess_tests";
+        BOOST_CHECK_MESSAGE(!boost::filesystem::exists(db_dir), "must remove dir " + db_dir.string() + " first");
+
+        BOOST_CHECK_NO_THROW(boost::filesystem::create_directory(db_dir));
+    }
+    ~FDBAccessTests() {
+        BOOST_TEST_MESSAGE( "teardown FDBAccessTests" );
+        BOOST_CHECK_NO_THROW(boost::filesystem::remove_all(db_dir));
+    }
+
+    boost::filesystem::path root_dir;
+    boost::filesystem::path db_dir;
+};
+
+BOOST_FIXTURE_TEST_SUITE(dbaccess_tests, FDBAccessTests)
+//BOOST_AUTO_TEST_SUITE(dbaccess_tests)
 
 BOOST_AUTO_TEST_CASE(dbaccess_test)
 {
     bool isWipe = true;
     shared_ptr<CDBAccess> pDBAccess = make_shared<CDBAccess>(
-        DBNameType::ACCOUNT, false, isWipe);
+        db_dir, DBNameType::ACCOUNT, false, isWipe);
     const dbk::PrefixType prefix = dbk::REGID_KEYID;
     map<string, string> mapData;
     mapData["regid-1"] = "keyid-1";
@@ -39,14 +63,15 @@ BOOST_AUTO_TEST_CASE(dbaccess_test)
 BOOST_AUTO_TEST_SUITE_END()
 
 
-BOOST_AUTO_TEST_SUITE(dbcache_tests)
+BOOST_FIXTURE_TEST_SUITE(dbcache_tests, FDBAccessTests)
+//BOOST_AUTO_TEST_SUITE(dbcache_tests)
 
 BOOST_AUTO_TEST_CASE(dbcache_multi_value_Level1_test)
 {
     const bool isWipe = true;
     const dbk::PrefixType prefix = dbk::REGID_KEYID;
     shared_ptr<CDBAccess> pDBAccess = make_shared<CDBAccess>(
-        DBNameType::ACCOUNT, false, isWipe);
+        db_dir, DBNameType::ACCOUNT, false, isWipe);
 
     auto pDBCache = make_shared< CCompositeKVCache<prefix, string, string> >(pDBAccess.get());
     pDBCache->SetData("regid-1", "keyid-1");
@@ -68,7 +93,7 @@ BOOST_AUTO_TEST_CASE(dbcache_multi_value_Level3_test)
     const bool isWipe = true;
     const dbk::PrefixType prefix = dbk::REGID_KEYID;
     shared_ptr<CDBAccess> pDBAccess = make_shared<CDBAccess>(
-        DBNameType::ACCOUNT, false, isWipe);
+        db_dir, DBNameType::ACCOUNT, false, isWipe);
 
     auto pDBCache1 = make_shared< CCompositeKVCache<prefix, string, string> >(pDBAccess.get());
     auto pDBCache2 = make_shared< CCompositeKVCache<prefix, string, string> >(pDBCache1.get());
@@ -104,7 +129,7 @@ BOOST_AUTO_TEST_CASE(dbcache_scalar_value_Level3_test)
     const bool isWipe = true;
     const dbk::PrefixType prefix = dbk::REGID_KEYID;
     shared_ptr<CDBAccess> pDBAccess = make_shared<CDBAccess>(
-        DBNameType::ACCOUNT, false, isWipe);
+        db_dir, DBNameType::ACCOUNT, false, isWipe);
 
     auto pDBCache1 = make_shared< CSimpleKVCache<prefix, string> >(pDBAccess.get());
     auto pDBCache2 = make_shared< CSimpleKVCache<prefix, string> >(pDBCache1.get());
