ad1f876a0fe5a03cdc104c772741812f5e6b5623
chenxinhua2018 <15220088021@163.com>
2019-12-17 09:46:07 +0800

    Improve wasm native contract structs

--- src/vm/wasm/modules/dex_contract.hpp
+++ src/vm/wasm/modules/dex_contract.hpp
@@ -8,6 +8,7 @@
 #include "wasm/wasm_context.hpp"
 #include "wasm/types/asset.hpp"
 #include "wasm/abi_def.hpp"
+#include "native_contract_base.hpp"
 
 using namespace std;
 using namespace wasm;
@@ -16,7 +17,32 @@ namespace dex {
 
 ////////////////////////////////////////////////////////////////////////////////
 ////// dex operator
+
+//       field_name       type_name     type             separator          description
+//       ----------    ------------ -------------     ---------------    -------------------
+#define DEX_OPERATOR_REGISTER(DEFINE, SEPARATOR, SEPARATOR_END) \
+    DEFINE( registrant, "name",     uint64_t)           SEPARATOR()       /* from name */ \
+    DEFINE( fee,        "asset",    wasm::asset)        SEPARATOR()       /* from nonce  */ \
+    DEFINE( owner,      "name",     uint64_t)           SEPARATOR()       /* owner  */ \
+    DEFINE( matcher,    "name",     uint64_t)           SEPARATOR()       /* matcher  */ \
+    DEFINE( name,       "string",   string)             SEPARATOR()       /* name  */ \
+    DEFINE( portal_url, "string",   string)             SEPARATOR()       /* portal url  */ \
+    DEFINE( memo,       "string",   string)             SEPARATOR_END()   /* memo  */
+
+#define DEX_OPERATOR_UPDATE(DEFINE, SEPARATOR, SEPARATOR_END) \
+    DEFINE( registrant, "name",     uint64_t)           SEPARATOR()       /* from name */ \
+    DEFINE( fee,        "asset",    wasm::asset)        SEPARATOR()       /* from nonce  */ \
+    DEFINE( id,         "uint32",   uint32_t)           SEPARATOR()       /* exid  */ \
+    DEFINE( owner,      "name?",    optional<uint64_t>) SEPARATOR()       /* owner  */ \
+    DEFINE( matcher,    "name?",    optional<uint64_t>) SEPARATOR()       /* matcher  */ \
+    DEFINE( name,       "string?",  optional<string>)   SEPARATOR()       /* name */ \
+    DEFINE( portal_url, "string?",  optional<string>)   SEPARATOR()       /* portal url */ \
+    DEFINE( memo,       "string?",  optional<string>)   SEPARATOR_END()   /* memo  */
+
     const static uint64_t dex_operator     = N(dexoperator);
+
+    DEFINE_WASM_NATIVE_STRUCT(operator_register_t, DEX_OPERATOR_REGISTER)
+    DEFINE_WASM_NATIVE_STRUCT(operator_update_t, DEX_OPERATOR_UPDATE)
 
     inline wasm::abi_def get_operator_abi() {
 
@@ -26,31 +52,15 @@ namespace dex {
             abi.version = "wasm::abi/1.0";
         }
 
+
         abi.structs = {
             //  name         base           fields
             //------------  --------  -------------------------
             {
-                "register",  "",   {    //field_name    type
-                                        {"registrant",  "name"},
-                                        {"fee",         "asset"},
-                                        {"owner",       "name"},
-                                        {"matcher",     "name"},
-                                        {"name",        "string"},
-                                        {"portal_url",  "string"},
-                                        {"memo",        "string"}
-                                    }
+                "register",  "",        operator_register_t::get_abi_fields()
             },
             {
-                "update",  "",      {    //field_name    type
-                                        {"registrant",  "name"},
-                                        {"fee",         "asset"},
-                                        {"id",          "uint32"},
-                                        {"owner",       "name?"},
-                                        {"name",        "string?"},
-                                        {"matcher",     "name?"},
-                                        {"portal_url",  "string?"},
-                                        {"memo",        "string?"}
-                                    }
+                "update",  "",          operator_update_t::get_abi_fields()
             }
         };
 
@@ -71,60 +81,21 @@ namespace dex {
 ////////////////////////////////////////////////////////////////////////////////
 ////// dex order
 
-//       field_name       type_name     type                        description
-//       ----------    ------------ -------------  -----------------------------------
-#define WASM_STRUCT_ORDERS(DEFINE, SEPARATOR, SEPARATOR_END) \
-    DEFINE( from,         "name",      uint64_t)    SEPARATOR()    /* from name */ \
-    DEFINE( from_nonce,   "uint64",    uint64_t)    SEPARATOR()     /* from nonce  */ \
-    DEFINE( exid,         "uint32",    uint32_t)    SEPARATOR()     /* exid  */ \
-    DEFINE( fee_rate,     "uint64",    uint64_t)    SEPARATOR()     /* fee rate  */ \
-    DEFINE( order_type,   "uint8",     uint8_t)     SEPARATOR()     /* order type  */ \
-    DEFINE( order_side,   "uint8",     uint8_t)     SEPARATOR()     /* order side  */ \
-    DEFINE( coin,        "asset",      wasm::asset) SEPARATOR()     /* coins  */ \
-    DEFINE( asset,       "asset",      wasm::asset) SEPARATOR()    /* assets  */ \
-    DEFINE( price,        "uint64",    uint64_t)    SEPARATOR()    /* price  */ \
-    DEFINE( memo,         "string",    string)      SEPARATOR_END()    /* memo  */ \
+//       field_name       type_name     type             separator          description
+//       ----------    ------------ -------------     ---------------    -------------------
+#define DEX_ORDER_CREATE(DEFINE, SEPARATOR, SEPARATOR_END) \
+    DEFINE( from,           "name",      uint64_t)    SEPARATOR()       /* from name */ \
+    DEFINE( from_nonce,     "uint64",    uint64_t)    SEPARATOR()       /* from nonce  */ \
+    DEFINE( exid,           "uint32",    uint32_t)    SEPARATOR()       /* exid  */ \
+    DEFINE( fee_rate,       "uint64",    uint64_t)    SEPARATOR()       /* fee rate  */ \
+    DEFINE( order_type,     "uint8",     uint8_t)     SEPARATOR()       /* order type  */ \
+    DEFINE( order_side,     "uint8",     uint8_t)     SEPARATOR()       /* order side  */ \
+    DEFINE( coin,           "asset",     wasm::asset) SEPARATOR()       /* coins  */ \
+    DEFINE( asset,          "asset",     wasm::asset) SEPARATOR()       /* assets  */ \
+    DEFINE( price,          "uint64",    uint64_t)    SEPARATOR()       /* price  */ \
+    DEFINE( memo,           "string",    string)      SEPARATOR_END()   /* memo  */ \
 
-    #define DEFINE_STRUCT_FIELD_ENUM(field_name, type_name, type) __enum_##field_name
-    #define DEFINE_STRUCT_TYPE(field_name, type_name, type) type
-    #define DEFINE_STRUCT_ABI_FIELD(field_name, type_name, type) { #field_name, type_name }
-    #define SEPARATOR_END_EMPTY()
-    #define SEPARATOR_COMMA() ,
-    #define DEFINE_STRUCT_FIELD_FUNC(field_name, type_name, type) \
-    type & field_name() { \
-        return std::get<__field_enum::__enum_##field_name>(_data); \
-    }
-
-    class order_t {
-    public:
-        enum __field_enum {
-            WASM_STRUCT_ORDERS(DEFINE_STRUCT_FIELD_ENUM, SEPARATOR_COMMA, SEPARATOR_END_EMPTY)
-        };
-
-        typedef std::tuple<
-            WASM_STRUCT_ORDERS(DEFINE_STRUCT_TYPE, SEPARATOR_COMMA, SEPARATOR_END_EMPTY)
-        > pack_type;
-
-        WASM_STRUCT_ORDERS(DEFINE_STRUCT_FIELD_FUNC, SEPARATOR_END_EMPTY, SEPARATOR_END_EMPTY)
-
-        static vector <field_def> get_abi_fields() {
-            return {
-                //DEFINE_STRUCT_ABI_FIELDS(aaa, "abc", int)
-                WASM_STRUCT_ORDERS(DEFINE_STRUCT_ABI_FIELD, SEPARATOR_COMMA, SEPARATOR_END_EMPTY)
-            };
-        };
-
-        void unpack_from_bin(const std::vector<char> &bin_data) {
-            wasm::unpack<pack_type>(bin_data, _data);
-        }
-
-        std::vector<char> pack_to_bin() {
-            return wasm::pack<pack_type>(_data);
-        }
-    private:
-        pack_type _data;
-    };
-
+    DEFINE_WASM_NATIVE_STRUCT(order_t, DEX_ORDER_CREATE)
 
     const static uint64_t dex_order     = N(dex.order);
 
