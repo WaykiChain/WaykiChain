9c5606fa2120c083faf4c6641344731a1a225a87
tigerich <tigerich@163.com>
2020-02-26 16:17:26 +0800

    improve ValueFromAmount return type

--- src/commons/util/util.h
+++ src/commons/util/util.h
@@ -462,6 +462,9 @@ inline std::string StrToUpper(const std::string &str) {
     return ret;
 }
 
-json_spirit::Value ValueFromAmount(uint64_t amount) { return (double)amount / (double)COIN; }
+json_spirit::Value ValueFromAmount(uint64_t amount) { 
+    uint64_t val = (double)amount / (double)COIN;
+    return val;
+}
 
 #endif
