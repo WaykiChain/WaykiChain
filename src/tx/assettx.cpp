fc98bafc6661767fab3858b95f4d574450542548
richardchen <tigerich@163.com>
2020-03-05 20:51:45 +0800

    fix compile issue

--- src/tx/assettx.cpp
+++ src/tx/assettx.cpp
@@ -115,7 +115,7 @@ bool CAssetIssueTx::CheckTx(CTxExecuteContext &context) {
     IMPLEMENT_CHECK_TX_REGID(txUid);
     if (!CheckFee(context)) return false;
 
-    auto symbolErr = CAsset::CheckSymbol(asset.asset_symbol);
+    auto symbolErr = CAssetIssueTx::CheckSymbol(asset.asset_symbol);
     if (symbolErr) {
         return state.DoS(100, ERRORMSG("CAssetIssueTx::CheckTx, invlid asset symbol! %s", *symbolErr),
             REJECT_INVALID, "invalid-asset-symbol");
