cd20bf2b2ea60b810d2902579f19f979de249c07
chenxinhua2018 <15220088021@163.com>
2020-03-06 12:17:56 +0800

    Revert "fix leb128"
    This reverts commit 8f1185e69446983bac2132adfe4ec23b258496c0.

--- src/commons/leb128.h
+++ src/commons/leb128.h
@@ -65,9 +65,7 @@ public:
     friend bool operator>(const CFixedLeb128 &a, const CFixedLeb128 &b) { return a.value > b.value; }
 };
 
-using CFixedUInt64 = CFixedLeb128<uint64_t>;
 using CFixedUInt32 = CFixedLeb128<uint32_t>;
-using CFixedUInt16 = CFixedLeb128<uint16_t>;
 
 #define READWRITE_FIXED_UINT32(n) READWRITE_FUNC(n, CFixedUInt32::SerReadWrite)
 #define READWRITE_FIXED_UINT16(n) READWRITE_FUNC(n, CFixedUInt16::SerReadWrite)
