d1ea516a4d7d095e5af76330f8aa88096c9ba85a
chenxinhua2018 <15220088021@163.com>
2020-03-06 12:17:53 +0800

    Revert "update"
    This reverts commit 941b6bffd137cb45155f22bb4cd10d2171d565dd.

--- src/commons/leb128.h
+++ src/commons/leb128.h
@@ -13,54 +13,12 @@
 
 // fixed size of LEB128(Little Endian Base 128)
 // @see LEB128: https://en.wikipedia.org/wiki/LEB128
-template<typename I, typename UnsignedInt = I>
+template<typename I>
 class CFixedLeb128 {
 public:
     I value;
-    static_assert(sizeof(I) == sizeof(UnsignedInt), "sizeof(I) == sizeof(UnsignedInt)");
     static const uint32_t SIZE = (sizeof(I) * 8 + 6) / 7;
-
-    template<typename Stream>
-    static inline unsigned int SerReadWrite(Stream& s, const I& v, int nType, int nVersion, CSerActionGetSerializeSize ser_action) {
-        return SIZE; // hex lenth
-    }
-
-    template<typename Stream>
-    static inline unsigned int SerReadWrite(Stream& s, const I& v, int nType, int nVersion, CSerActionSerialize ser_action) {
-        SerializeValue(s, v);
-        return 0;
-    }
-
-    template<typename Stream>
-    static inline unsigned int SerReadWrite(Stream& s, I& v, int nType, int nVersion, CSerActionUnserialize ser_action) {
-        UnserializeValue(s, v);
-        return 0;
-    }
-
-    template<typename Stream>
-    static inline void SerializeValue(Stream& s, const I& v)
-    {
-        uint8_t bytes[SIZE] = {0};
-        UnsignedInt uv = v;
-        for(uint32_t i = 0; i < SIZE; i++) {
-            // code the int from high to low order
-            bytes[SIZE - i - 1] = (uint8_t)(uv & 0x7F);
-            uv = uv >> 7;
-        }
-        s.write((char*)bytes, SIZE);
-    }
-
-    template<typename Stream>
-    static inline void UnserializeValue(Stream& s, I& v)
-    {
-        uint8_t bytes[SIZE] = {0};
-        s.read((char*)bytes, SIZE);
-        v = 0;
-        for(uint32_t i = 0; i < SIZE; i++) {
-            // the bytes store the int from high to low order
-            v = (v << 7) | bytes[i];
-        }
-    }
+    typedef typename std::make_unsigned<I>::type UnsignedInt; // supports signed int type
 public:
     CFixedLeb128(): value(0) {}
     explicit CFixedLeb128(const I &valueIn): value(valueIn) {}
@@ -73,18 +31,30 @@ public:
     template<typename Stream>
     void Serialize(Stream& s, int nType, int nVersion) const
     {
-        SerializeValue(s, value);
+        uint8_t bytes[SIZE] = {0};
+        UnsignedInt v = value;
+        for(uint32_t i = 0; i < SIZE; i++) {
+            // code the int from high to low order
+            bytes[SIZE - i - 1] = (uint8_t)(v & 0x7F);
+            v = v >> 7;
+        }
+        s.write((char*)bytes, SIZE);
     }
 
     template<typename Stream>
     void Unserialize(Stream& s, int nType, int nVersion)
     {
-        UnserializeValue(s, value);
+        uint8_t bytes[SIZE] = {0};
+        s.read((char*)bytes, SIZE);
+        value = 0;
+        for(uint32_t i = 0; i < SIZE; i++) {
+            // the bytes store the int from high to low order
+            value = (value << 7) | bytes[i];
+        }
     }
 
     bool IsEmpty() const { return value == 0; }
     void SetEmpty() { value = 0; }
-    string ToString() const { return std::to_string(value); }
 
     bool operator==(I i) { return value == i; }
     bool operator<(I i) { return value < i; }
