50400cf6cb2fdbb7b75bb328b8d75c0f7a916de1
chenxinhua2018 <15220088021@163.com>
2020-03-26 17:38:28 +0800

    fix error msg in GetBcoinMedianPrice()

--- src/tx/cdptx.cpp
+++ src/tx/cdptx.cpp
@@ -45,8 +45,8 @@ static bool GetBcoinMedianPrice(CBaseTx &tx, CTxExecuteContext &context, const C
     PriceCoinPair priceCoinPair(cdpCoinPair.bcoin_symbol, quoteSymbol);
     context.pCw->priceFeedCache.GetMedianPriceDetail(priceCoinPair, priceDetail);
     if (priceDetail.price == 0 || !priceDetail.IsActive(context.height, priceTimeoutBlocks)) {
-        return context.pState->DoS(100, ERRORMSG("CCDPStakeTx::ExecuteTx, the price of %s:%s is empty or inactive! "
-                "price=%llu, last_feed_height=%u", CoinPairToString(priceCoinPair),
+        return context.pState->DoS(100, ERRORMSG("CCDPStakeTx::ExecuteTx, the price of %s is empty or inactive! "
+                "price=%llu, last_feed_height=%u", CoinPairToString(priceCoinPair), priceDetail.price,
                 priceDetail.last_feed_height), REJECT_INVALID, "invalid-bcoin-price");
     }
     bcoinPrice = priceDetail.price;
