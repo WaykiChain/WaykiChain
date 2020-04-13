330ddc058cd476de9795df82f9d084b96e22ab72
chenxinhua2018 <15220088021@163.com>
2020-04-13 21:07:13 +0800

    fix the warning of swtich case in hash.cc

--- src/persistence/leveldb/util/hash.cc
+++ src/persistence/leveldb/util/hash.cc
@@ -48,12 +48,10 @@ uint32_t Hash(const char* data, size_t n, uint32_t seed) {
   switch (limit - data) {
     case 3:
       h += data[2] << 16;
-      FALLTHROUGH_INTENDED;
-      // [[fallthrough]]
+      [[fallthrough]];
     case 2:
       h += data[1] << 8;
-      FALLTHROUGH_INTENDED;
-      // [[fallthrough]]
+      [[fallthrough]];
     case 1:
       h += data[0];
       h *= m;
