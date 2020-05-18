3ffed0d3cc74edd9cfaf9e9da0a2d7e4c98dacd9
chenxinhua2018 <15220088021@163.com>
2020-05-18 14:44:22 +0800

    improve build-waykicoind.sh

--- docker/bin/build-waykicoind.sh
+++ docker/bin/build-waykicoind.sh
@@ -10,11 +10,7 @@ BRANCH=$3
 [ -z "$BRANCH" ] && BRANCH='release'
 
 echo "Setting SOURCE_BRANCH to ${BRANCH}"
-sed -i '' "s@^ENV SOURCE_BRANCH \"release\"@ENV SOURCE_BRANCH \"${BRANCH}\"@g" Dockerfile
-
-COMMIT=$(curl -s 'https://api.github.com/repos/WaykiChain/WaykiChain/commits' | grep sha | head -1 | cut -c 13-20)
-echo "Setting SOURCE_COMMIT: ${COMMIT}"
-sed -i '' "s@SOURCE_COMMIT@${COMMIT}@g" Dockerfile
+sed -i "s@^ENV SOURCE_BRANCH \"release\"@ENV SOURCE_BRANCH \"${BRANCH}\"@g" Dockerfile
 
 docker build \
     --build-arg debug=$DEBUG \
