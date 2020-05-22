3b6f05f5862772a1eb8ff0fe5c4c77d16751218e
chenxinhua2018 <15220088021@163.com>
2020-05-22 19:39:34 +0800

    remove RPCTypeCheck() in submittx

--- src/rpc/rpcsubmittx.cpp
+++ src/rpc/rpcsubmittx.cpp
@@ -287,7 +287,6 @@ Value submittx( const Array &params, bool fHelp ) {
 
     //WASM_TRACE(params[1].get_str())
     RESPONSE_RPC_HELP( fHelp || params.size() < 4 || params.size() > 5 , wasm::rpc::submit_tx_rpc_help_message)
-    RPCTypeCheck(params, list_of(str_type)(str_type)(str_type)(str_type)(str_type));
 
     try {
         auto db_account  = pCdMan->pAccountCache;
