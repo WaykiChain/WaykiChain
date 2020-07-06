a1d713a37d12fb04b4f4893786081005a4d5df82
chenxinhua2018 <15220088021@163.com>
2020-07-06 19:30:29 +0800

    remove unused code

--- src/vm/wasm/wasm_log.hpp
+++ src/vm/wasm/wasm_log.hpp
@@ -2,20 +2,4 @@
 
 #include "logging.h"
 
-// #include <cstdint>
-// #include <string>
-
-// using namespace std;
-
-// #define WASM_FUNCTION_PRINT_LENGTH 64
-
-// #define WASM_TRACE( ... ) {                  \
-//    string msg = tfm::format( __VA_ARGS__ );  \
-//    std::ostringstream o;                     \
-//    o << __FILE__ << ":" << __LINE__          \
-//              << ":[" << __FUNCTION__ << "]"; \
-//    while (o.str().size() < WASM_FUNCTION_PRINT_LENGTH) \
-//         o << " ";                             \
-//     std::cout << o.str() << msg << std::endl;}
-
 #define WASM_TRACE( ... ) LogPrint(BCLog::WASM, __VA_ARGS__);
