2e8dc328be7b5b10cbc2539d7ca17e75f7a60577
Dragon Master <tigerich@users.noreply.github.com>
2020-07-15 14:55:56 +0800

    Update build-waykicoind.sh

--- docker/bin/build-waykicoind.sh
+++ docker/bin/build-waykicoind.sh
@@ -4,15 +4,14 @@ DEBUG=$1
 [ -z "$DEBUG" ] && DEBUG=''
 
 VER=$2
-[ -z "$VER" ] && VER='1.3'
+[ -z "$VER" ] && VER='3.0'
 
 BRANCH=$3
-[ -z "$BRANCH" ] && BRANCH='release'
+[ -z "$BRANCH" ] && BRANCH='rel/v3.0.0'
 
 echo "Setting SOURCE_BRANCH to ${BRANCH}"
 sed -i "s@^ENV SOURCE_BRANCH \"release\"@ENV SOURCE_BRANCH \"${BRANCH}\"@g" Dockerfile
 
 docker build \
     --build-arg debug=$DEBUG \
-    --build-arg branch=$BRANCH \
     -t wicc/waykicoind:$VER .
